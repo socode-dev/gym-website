91a9d30a0c80398af09484ec678aa3135ab8d278
Big Sam <stosin525@gmail.com>
2025-03-19 13:23:20 -0700

    Changed my navigation color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ const Header = ({ menuOpen, setMenuOpen, DumbbellIcon }) => {
   };
 
   return (
-    <header className="w-full bg-black flex py-2.5 justify-center items-center border-b-4 border-solid border-b-neutral-800 shadow-lg shadow-neutral-700">
+    <header className="w-full bg-black flex py-2.5 justify-center items-center border-b-4 border-solid border-b-neutral-800 shadow-lg shadow-neutral-800">
       <section
         id="desktopMenu"
         className="flex justify-between items-center w-full pr-4"
@@ -38,7 +38,7 @@ const Header = ({ menuOpen, setMenuOpen, DumbbellIcon }) => {
         >
           <MenuTwoToneIcon
             fontSize="large"
-            className="text-white dark:text-black hover:opacity-80"
+            className="text-white hover:opacity-80"
           />
         </div>
       </section>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,19 +3,19 @@ import { NavLink } from "react-router-dom";
 const NavLinks = ({ flexDirection }) => {
   return (
     <ul className={`flex ${flexDirection}`}>
-      <li className="text-xl lg:text-sm text-white dark:text-black w-fit py-2.5 px-4 rounded-3xl transition duration-300 hover:bg-red-600 active:bg-red-800">
+      <li className="text-xl lg:text-sm text-white w-fit py-2.5 px-4 rounded-3xl transition duration-300 hover:bg-red-600 active:bg-red-800">
         <NavLink to="/">Home</NavLink>{" "}
       </li>
-      <li className="text-xl lg:text-sm text-white dark:text-black w-fit py-2.5 px-4 rounded-3xl transition duration-300 hover:bg-red-600 active:bg-red-800">
+      <li className="text-xl lg:text-sm text-white w-fit py-2.5 px-4 rounded-3xl transition duration-300 hover:bg-red-600 active:bg-red-800">
         <NavLink to="about">About</NavLink>{" "}
       </li>
-      <li className="text-xl lg:text-sm text-white dark:text-black w-fit py-2.5 px-4 rounded-3xl transition duration-300 hover:bg-red-600 active:bg-red-800">
+      <li className="text-xl lg:text-sm text-white w-fit py-2.5 px-4 rounded-3xl transition duration-300 hover:bg-red-600 active:bg-red-800">
         <NavLink to="services">Services</NavLink>{" "}
       </li>
-      <li className="text-xl lg:text-sm text-white dark:text-black w-fit py-2.5 px-4 rounded-3xl transition duration-300 hover:bg-red-600 active:bg-red-800">
+      <li className="text-xl lg:text-sm text-white w-fit py-2.5 px-4 rounded-3xl transition duration-300 hover:bg-red-600 active:bg-red-800">
         <NavLink to="membership">Membership</NavLink>{" "}
       </li>
-      <li className="text-xl lg:text-sm text-white dark:text-black w-fit py-2.5 px-4 rounded-3xl transition duration-300 hover:bg-red-600 active:bg-red-800">
+      <li className="text-xl lg:text-sm text-white w-fit py-2.5 px-4 rounded-3xl transition duration-300 hover:bg-red-600 active:bg-red-800">
         <NavLink to="contact">Contact</NavLink>{" "}
       </li>
     </ul>
